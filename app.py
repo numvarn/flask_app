65be517bc453532c3b28849e3bbfd4ffed5eae0a
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2022-01-29 19:04:38 +0700

    update line-bot-sdk

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
+from linebot import *
+
 app = Flask(__name__)
+
+line_bot_api = LineBotApi('xxxxxx')
+handler = WebhookHandler('xxxxxx')
 
 
 @app.route('/')
@@ -10,8 +15,21 @@ def home():
 @app.route("/callback", methods=['POST'])
 def callback():
     body = request.get_data(as_text=True)
-    print(body['queryResult']['queryText'])
-    return body['queryResult']['queryText']
+    # print(body)
+    req = request.get_json(silent=True, force=True)
+    intent = req["queryResult"]["intent"]["displayName"]
+    text = req['originalDetectIntentRequest']['payload']['data']['message']['text']
+    reply_token = req['originalDetectIntentRequest']['payload']['data']['replyToken']
+    id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
+    disname = line_bot_api.get_profile(id).display_name
+
+    print('id = ' + id)
+    print('name = ' + disname)
+    print('text = ' + text)
+    print('intent = ' + intent)
+    print('reply_token = ' + reply_token)
+
+    return 'OK'
 
 
 if __name__ == '__main__':
